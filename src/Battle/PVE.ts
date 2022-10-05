1304e3834aaad52f68f2a555c927b004a7693ac1
Wendril-Costa <wendrild.c.n@gmail.com>
2022-10-05 20:12:45 -0300

    Feito - Requisito 11

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,21 @@
+import Fighter, { SimpleFighter } from '../Fighter';
+import Battle from './Battle';
+
+class PVE extends Battle {
+  constructor(private _player: Fighter, private _monsters: SimpleFighter[]) {
+    super(_player);
+  }
+  
+  fight(): number {
+    this._monsters.map((monster: SimpleFighter) => {
+      if (this._player.lifePoints > -1 && monster.lifePoints > -1) {
+        this._player.attack(monster);
+        monster.attack(this._player);
+      }
+      return 1;
+    });
+    return super.fight();
+  }
+}
+
+export default PVE;
