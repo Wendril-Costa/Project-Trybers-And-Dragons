b886775088e2b1d618b783c276719150ae25bfe6
Wendril-Costa <wendrild.c.n@gmail.com>
2022-10-05 19:46:38 -0300

    Feito - Requisito 10

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,21 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+class PVP extends Battle {
+  constructor(
+    private _playerOne: Fighter,
+    private _playerTwo: Fighter,
+  ) {
+    super(_playerOne);
+  }
+
+  fight(): number {
+    if (this._playerOne.lifePoints > -1 && this._playerTwo.lifePoints > -1) {
+      this._playerOne.attack(this._playerTwo);
+      this._playerTwo.attack(this._playerOne);
+    }
+    return super.fight();
+  }
+}
+
+export default PVP;
